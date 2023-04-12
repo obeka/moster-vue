d0169a19bd397e0c786e1ba9d5613eb410cad8e4
omerbkk06@gmail.com <omerbkk06@gmail.com>
2023-04-12 12:23:19 +0200

    Bind the methods for special attack

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const app = Vue.createApp({
     return {
       playerHealth: 100,
       monsterHealth: 100,
+      currentRound: 0,
     };
   },
   computed: {
@@ -16,9 +17,13 @@ const app = Vue.createApp({
     playerBarStyles() {
       return { width: this.playerHealth + "%" };
     },
+    mayUseSpecialAttack() {
+      return this.currentRound % 3 !== 0;
+    },
   },
   methods: {
     attackMonster() {
+      this.currentRound++;
       const attackedValue = getRandomValue(5, 12);
       this.monsterHealth -= attackedValue;
       this.attackPlayer();
@@ -26,6 +31,12 @@ const app = Vue.createApp({
     attackPlayer() {
       const attackedValue = getRandomValue(8, 15);
       this.playerHealth -= attackedValue;
+    },
+    specialAttackMonster() {
+      this.currentRound++;
+      const attackedValue = getRandomValue(10, 25);
+      this.monsterHealth -= attackedValue;
+      this.attackPlayer();
     },
   },
 });
