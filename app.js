1623f6171ce129083863dd7c51669e76e7bb9814
omerbkk06@gmail.com <omerbkk06@gmail.com>
2023-04-12 12:07:49 +0200

    Created first methods

---
+++ app.js
@@ -0,0 +1,25 @@
+function getRandomValue(min, max) {
+  return Math.floor(Math.random() * (max - min) + min);
+}
+
+const app = Vue.createApp({
+  data() {
+    return {
+      playerHealth: 100,
+      monsterHealth: 100,
+    };
+  },
+  methods: {
+    attackMonster() {
+      const attackedValue = getRandomValue(5, 12);
+      this.monsterHealth -= attackedValue;
+      this.attackPlayer();
+    },
+    attackPlayer() {
+      const attackedValue = getRandomValue(8, 15);
+      this.playerHealth -= attackedValue;
+    },
+  },
+});
+
+app.mount("game");
